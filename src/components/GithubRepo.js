e6bc88b8888a35afc6e2202e8391e3f6e0eefe17
Amelie Pira <amelie.pira@gmail.com>
2022-10-25 16:47:34 +0100

    aligned after useEffect

--- src/components/GithubRepo.js
+++ src/components/GithubRepo.js
@@ -6,6 +6,7 @@ export const GithubRepo = () => {
     repos: undefined,
     selectedRepo: undefined,
     username: "",
+    isSearching: false,
   };
   //function reducer
   const reducer = (state, action) => {
@@ -16,6 +17,12 @@ export const GithubRepo = () => {
         return { ...state, selectedRepo: action.selection };
       case "username":
         return { ...state, username: action.username };
+      case "usernamefirst":
+        return {
+          ...state,
+          username: action.username,
+          isSearching: action.isSearching,
+        };
       default:
         throw new Error();
     }
@@ -27,6 +34,13 @@ export const GithubRepo = () => {
   useEffect(() => {
     getReposAndDispatch("am0031");
   }, []);
+
+  //useEffect for username change - useEffect #2
+  useEffect(() => {
+    if (state.isSearching === true) {
+      getReposAndDispatch(state.username);
+    }
+  }, [state.username, state.isSearching]);
 
   //function to get repos
   const getReposAndDispatch = async (username) => {
@@ -40,15 +54,17 @@ export const GithubRepo = () => {
     dispatch({ type: "repos", newRepos });
   };
 
-  //function to get the info from the form and do the search
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    getReposAndDispatch(state.username);
-  };
-
   //function to handle input field change
   const handleChange = (event) => {
-    dispatch({ type: "username", username: event.target.value });
+    if (!state.isSearching) {
+      dispatch({
+        type: "usernamefirst",
+        username: event.target.value,
+        isSearching: true,
+      });
+    } else {
+      dispatch({ type: "username", username: event.target.value });
+    }
   };
 
   //function to handle click on a repo to see more details
@@ -59,18 +75,42 @@ export const GithubRepo = () => {
     dispatch({ type: "selected", selection });
   };
 
+  //function to set time style
+  const setTimeStyle = (selectedRepo) => {
+    if (selectedRepo) {
+      const updateDay = new Date(selectedRepo.updated_at);
+      const timeframe = Math.floor(
+        (new Date() - updateDay) / (1000 * 3600 * 24)
+      );
+
+      if (timeframe < 7) {
+        return "green";
+      } else if (timeframe < 21) {
+        return "orange";
+      } else {
+        return "red";
+      }
+    }
+  };
+
   return (
-    <div>
-      <h1>Public repos by user</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          id="search-input"
-          value={state.username}
-          placeholder="Enter github username"
-          onChange={handleChange}
-        ></input>
-        <button id="submit-btn">Search</button>
-      </form>
+    <div style={{ marginLeft: "2rem" }}>
+      <h1>
+        Public repos by user - **This page is managed with useReducer and
+        useEffect**
+      </h1>
+      <div>
+        <h2>Search form</h2>
+        <form>
+          <input
+            style={{ width: "400px" }}
+            id="search-input"
+            value={state.username}
+            placeholder="Enter github username"
+            onChange={handleChange}
+          ></input>
+        </form>
+      </div>
       <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
         <div
           style={{
@@ -81,6 +121,7 @@ export const GithubRepo = () => {
           }}
         >
           <h2>Search results</h2>
+          {state.repos && <h3>{state.repos.length} repos</h3>}
           {state.repos && state.repos.length ? (
             state.repos.map((item) => {
               return (
@@ -103,7 +144,13 @@ export const GithubRepo = () => {
           {state.selectedRepo && (
             <>
               <h3>{state.selectedRepo.name}</h3>
-              <h3>Last updated: {state.selectedRepo.updated_at}</h3>
+              <h3
+                style={{
+                  color: `${setTimeStyle(state.selectedRepo)}`,
+                }}
+              >
+                Last updated: {state.selectedRepo.updated_at}
+              </h3>
               <h3>URL: {state.selectedRepo.url}</h3>
             </>
           )}
